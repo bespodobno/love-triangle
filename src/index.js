2fa7376c5c3790e70e1517410d85728162dbdb5f
bespodobno <nadzeya.tron@gmail.com>
2018-09-14 08:52:22 +0300

    task_2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var c = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+    
+    var j = preferences[i];
+    if (j - 1 > preferences.length) continue;
+    var z = preferences[j-1];
+    if (z - 1 > preferences.length) continue;
+    if (i === preferences[z-1]-1) {
+      c++;
+    }
+  }
+  return Math.floor(c / 3);
 };
